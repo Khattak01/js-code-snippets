d62578d8b70a9d851b4283d41b25922379f5be2a
Khattak01 <57499748+Khattak01@users.noreply.github.com>
2021-04-14 02:24:52 -0700

    version 2 of the findIndexOfObjectInArray() added

--- find-searchIndexOfObjectInArray.js
+++ find-searchIndexOfObjectInArray.js
@@ -6,23 +6,26 @@ const bookings = [
 ]
 
 //return index of the element if find else return -1 
-const findIndex = (booking) => bookings.findIndex((b, index) => {
-    if (b._id === booking._id) {
-        return true
-    }
+// const findIndex = (booking) => bookings.findIndex((b, index) => {
+//     if (b._id === booking._id) return true
+// })
+
+//version 2
+const findIndex = (list,ele) => list.findIndex((e, index) => {
+    if (e._id === ele._id) return true
 })
 
 //test 1
 let booking = { status: "pending", _id: "6055cb33062eb5153c089122", title: "title2", description: "test description", user: "id", team: "id" }
-console.log("index >>> ", findIndex(booking))
+console.log("index >>> ", findIndex(bookings,booking))
 //output : 1
 
 //test 2
 booking = { status: "rejected", _id: "6055cb33062eb5153c089198", title: "title3", description: "test description", user: "id", team: "id" }
-console.log("index >>> ", findIndex(booking))
+console.log("index >>> ", findIndex(bookings,booking))
 //output : -1
 
 //test 3
 const id = '6055cb3d062eb5153c089123'
-console.log("index >>> ", findIndex({ _id: id }))
+console.log("index >>> ", findIndex(bookings,{ _id: id }))
 //output : 2
